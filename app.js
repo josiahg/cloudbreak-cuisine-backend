8928995306cff08e8cbcfda155067fc028d89109
Josiah Goodson <jgoodson@hortonworks.com>
2019-01-04 13:34:46 -0500

    We got Swagger

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -11,6 +13,7 @@ app.use(bodyParser.json());
 const initDb = require('./src/pg-db').initDb;
 initDb();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(require('./src/routes'));
 app.use('/', router);
 
